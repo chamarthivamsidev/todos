f6ce80125ec0ff4d47707884e88c8520924017a1
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-02-06 13:09:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import { Todo } from "./components/Todo";
+// import "./App.css";
+import { Todos } from "./Todos";
+// import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div className="outterScreen">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todos.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+const axios = require("axios").default;
+
+export const Todos = () => {
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  const getData = () => {
+    axios
+      .get(`http://localhost:3002/todos?_page=${page}&_limit=4`)
+      .then((res) => {
+        let data = res.data;
+        setTodo(data);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="title">Todo...</h1>
+      <input type="text" className="inputTitle" />
+      <br />
+      <input
+        type="text"
+        className="inputBody"
+        placeholder="Add Task..."
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button
+        className="addBtn"
+        onClick={() => {
+          const list = { status: false, title: text };
+          axios({
+            method: "post",
+            url: "http://localhost:3002/todos",
+            data: list,
+          });
+          getData();
+        }}
+      >
+        Add
+      </button>
+      <br />
+      <div className="container">
+        {todo.map((e) => (
+          <div key={e.id} className="list_items">
+            {e.id} - {e.title} - {e.status ? "Done" : "Not Done"}
+          </div>
+        ))}
+      </div>
+      <button
+        className="prev"
+        onClick={() => {
+          if (page > 1) {
+            setPage(page - 1);
+          }
+        }}
+      >
+        Prev
+      </button>
+      <button
+        className="next"
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
